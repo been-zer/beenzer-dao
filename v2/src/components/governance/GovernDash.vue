562c5467a8621ca5b9145bdb8b1cd1745cbaaa8c
quantium <alexcollsoutumuro@gmail.com>
2023-01-12 12:58:18 +0100

    add govern ui

--- v2/src/components/governance/GovernDash.vue
+++ v2/src/components/governance/GovernDash.vue
@@ -1,31 +1,20 @@
 
-<script>
+<script lang="ts">
 import { ref } from 'vue';
 import click_sound from '../../assets/sounds/beep.mp3';
 import VotingArea from './VotingArea.vue';
 import ResultsArea from './ResultsArea.vue';
+import { useStore } from '../../services/store';
 
 export default {
   components: {
     VotingArea,
     ResultsArea
   },
-  props: [
-    'potSOL',
-    'wallet',
-    'balance',
-    'tickets',
-    'yourNumbers',
-    'yourProbability',
-    'yourROI'
-  ],
   methods: {
     commitNumber () {
       this.$emit('commit', this.commitNumber)
     },
-    checkBox ( checkbox ) {
-      this.checkbox = checkbox;
-    }
   },
   data() {
     return {
@@ -34,15 +23,46 @@ export default {
       commitHover: false,
       commiting: false,
     }
+  },
+  setup () {
+    const store = useStore();
+    const selTab = ref('Vote')
+    function onTab ( tab:string ) {
+      selTab.value = tab;
+    }
+
+    return {
+      store,
+      selTab,
+      onTab
+    }
   }
 }
 </script>
 <template>
 <div class="m-auto p-2 mt-0">
-  <div class="flex flex-wrap justify-center h-[840px] p-4 text-gray-600 rounded-xl text-center shadow-xl shadow-yellow-400" 
-  :class="this.$store.state.dark ? 'bg-gray-800' : 'bg-white'">
-    <VotingArea />
-    <ResultsArea />
+  <div class="h-[840px] rounded-xl text-center shadow-xl shadow-purple-400" >
+  <div class="grid grid-cols-3 justify-between">
+    <button class="text-center p-2 rounded-tl-xl"
+    :onclick="() => onTab('Vote')"
+    :class="selTab==='Vote' ? 'bg-green-500' : 'bg-transparent'">
+      Vote
+    </button>
+    <button class="text-center p-2"
+    :onclick="() => onTab('Results')"
+    :class="selTab==='Results' ? 'bg-green-500' : 'bg-transparent'" >
+      Results
+    </button>  
+    <button class="text-center p-2 rounded-tr-xl"
+    :onclick="() => onTab('Calendar')"
+    :class="selTab==='Calendar' ? 'bg-green-500' : 'bg-transparent'">
+      Calendar
+    </button>  
+  </div>
+  <div class="m-0 p-4 border border-green-500">
+  <VotingArea :class="selTab==='Vote' ? 'block' : 'hidden'"/>
+  <ResultsArea :class="selTab==='Results' ? 'block' : 'hidden'"/>
+  </div>
   </div>
 </div>
 
