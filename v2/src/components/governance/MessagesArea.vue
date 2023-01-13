b848333b54ae41ae67e94dc65a904d26f55ca433
quantium <alexcollsoutumuro@gmail.com>
2023-01-13 09:08:19 +0100

    finish VotingArea

--- v2/src/components/governance/MessagesArea.vue
+++ v2/src/components/governance/MessagesArea.vue
@@ -23,8 +23,15 @@ export default {
       // const date = new Date();
       messages.value.push({ 
         pubkey: shortWallet(store.state.pubkey, 4),
-        username: 'beenzer',
+        username: store.state.username,
         msg: msg.value, 
+        date: '2013',
+      });
+      msg.value = '';
+      messages.value.push({ 
+        pubkey: 'beenzer',
+        username: 'Beenzer bot',
+        msg: 'Sorry my dear! We are still working on the chat functionality. It will be reayd very soon! Thanks for your patience.', 
         date: '2013',
       });
     }
@@ -39,20 +46,21 @@ export default {
 } 
 </script>
 <template>
-<div class="p-2 w-[800px] relative rounded-tl-xl rounded-bl-xl shadow-inner"
+<div class="p-2 w-[800px] relative rounded-tl-xl rounded-bl-xl shadow-inner overflow-y-scroll"
 :class="store.state.dark ? 'bg-white/10 shadow-white/20' : 'bg-black/10 shadow-black/20'">
-  <div class="p-2 absolute bottom-2 left-1/2 transform -translate-x-1/2 text-gray-400">
-    <input type="text" class="w-full text-center text-sm p-1 px-24 rounded-xl border border-green-500"
+  <div class="p-2 fixed bottom-12 left-1/2 ml-10">
+    <input type="text" class="text-center text-sm p-1 px-2 w-[300px] rounded-xl border border-green-500"
     id="msg" key="msg"
     :class="store.state.dark ? 'bg-black/20' : 'bg-white/20'"
     v-on:keyup.enter="onEnter()"
     v-model="msg"
-    placeholder="✍🏽 Be polite and press enter"/>
+    placeholder="✍🏽 Be polite and press Enter"/>
   </div>
   <div v-for="x of messages" :key="x.msg"
   class="flex"
   :class="store.state.pubkey===x.pubkey ? 'flex-start' : 'flex-end'">
-    <div>
+  <div class="bg-black/20 p-2 px-8 my-2 rounded-xl rounded-bl-none min-w-[200px]">
+    <div class="text-white">
       {{ x.username }}
     </div>
     <div>
@@ -62,6 +70,7 @@ export default {
       {{ x.date }}
     </div>
   </div>
+</div>
 
 </div>
 </template>
--- v2/src/components/governance/VotingArea.vue
+++ v2/src/components/governance/VotingArea.vue
@@ -1,9 +1,10 @@
 
 <script>
-import { ref } from 'vue';
-import click_sound from '../../assets/sounds/beep.mp3';
+// import { ref } from 'vue';
+// import click_sound from '../../assets/sounds/beep.mp3';
 import CountDown from '../modules/CountDown';
 import motions from '../../../config/motions.json';
+import { useStore } from '../../services/store';
 
 export default {
   components: {
@@ -27,12 +28,75 @@ export default {
     }
   },
   setup () {
-
+    const store = useStore();
     const nf = Intl.NumberFormat();
 
+    const radialData = {         
+      series: [76],
+      chartOptions: {
+        chart: {
+          type: 'radialBar',
+          foreColor: '#7F7F7F',
+          size: '16px',
+          offsetY: -20,
+          sparkline: {
+            enabled: true
+          }
+        },
+        plotOptions: {
+          radialBar: {
+            startAngle: -90,
+            endAngle: 90,
+            track: {
+              background: "#7F7F7F",
+              strokeWidth: '97%',
+              margin: 5, // margin is in pixels
+              dropShadow: {
+                enabled: true,
+                top: 2,
+                left: 0,
+                color: '#16a34a',
+                opacity: 1,
+                blur: 2
+              }
+            },
+            dataLabels: {
+              name: {
+                show: false
+              },
+              value: {
+                offsetY: -2,
+                fontSize: '22px'
+              }
+            }
+          }
+        },
+        grid: {
+          padding: {
+            top: -10
+          }
+        },
+        fill: {
+          type: 'gradient',
+          colors: ['#16a34a'],
+          gradient: {
+            shade: 'dark',
+            type: 'horizontal',
+            shadeIntensity: 0.5,
+            gradientToColors: ['red', '#16a34a'],
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100]
+          }
+        }
+      },
+    }
     
     return { 
-      nf
+      store,
+      nf,
+      radialData
     }
   }
 }
@@ -48,32 +112,65 @@ export default {
       </p>
     </div>
     <div class="mt-4 px-8">
-      <div class="uppercase text-2xl tracking-widest font-semibold text-center p-4">
+      <div class="uppercase text-xl tracking-widest font-semibold text-center p-2 mb-4">
         {{ motions[0].prop }}
       </div>
-      <div class="p-4 pl-6 my-2 font-bold text-sm text-left w-full rounded-xl cursor-pointer border border-gray-500 hover:bg-green-500 hover:text-white"
-      :class="this.$store.state.dark ? 'bg-gray-600 hover:bg-gray-600' : 'bg-gray-50 hover:bg-gray-200'">
-        {{motions[0].a}}
+      <div class="my-2 flex w-full rounded-xl cursor-pointer border hover:bg-green-500 hover:text-white"
+        :class="store.state.dark ? 'bg-gray-800 border-gray-600' : 'bg-gray-200 border-gray-400'">
+        <div class="rounded-l-xl p-4 align-text-middle font-semibold w-12"
+        :class="store.state.dark ? 'bg-black/20 border-r border-gray-600' : 'bg-white/20 border-r border-gray-400'">
+          A
+        </div>
+        <div class="text-center w-full rounded-r-xl p-2 my-2 font-bold -ml-12">
+          {{motions[0].a}}
+        </div>  
       </div>
-      <div class="p-4 pl-6 my-2 font-bold text-sm text-left w-full rounded-xl cursor-pointer border border-gray-500 hover:bg-green-600 hover:border-yellow-500"
-      :class="this.$store.state.dark ? 'bg-gray-600 hover:bg-gray-600' : 'bg-gray-50 hover:bg-gray-200'">
-        {{motions[0].b}}
+      <div class="my-2 flex w-full rounded-xl cursor-pointer border hover:bg-green-500 hover:text-white"
+        :class="store.state.dark ? 'bg-gray-800 border-gray-600' : 'bg-gray-200 border-gray-400'">
+        <div class="rounded-l-xl p-4 align-text-middle font-semibold w-12"
+        :class="store.state.dark ? 'bg-black/20 border-r border-gray-600' : 'bg-white/20 border-r border-gray-400'">
+          B
+        </div>
+        <div class="text-center w-full rounded-r-xl p-2 my-2 font-bold -ml-12">
+          {{motions[0].b}}
+        </div>  
       </div>
-      <div class="p-4 pl-6 my-2 font-bold text-sm text-left w-full rounded-xl cursor-pointer border border-gray-500 hover:bg-green-600 hover:border-yellow-500"
-      :class="this.$store.state.dark ? 'bg-gray-600 hover:bg-gray-600' : 'bg-gray-50 hover:bg-gray-200'">
-        {{motions[0].c}}
+      <div class="my-2 flex w-full rounded-xl cursor-pointer border hover:bg-green-500 hover:text-white"
+        :class="store.state.dark ? 'bg-gray-800 border-gray-600' : 'bg-gray-200 border-gray-400'">
+        <div class="rounded-l-xl p-4 align-text-middle font-semibold w-12"
+        :class="store.state.dark ? 'bg-black/20 border-r border-gray-600' : 'bg-white/20 border-r border-gray-400'">
+          C
+        </div>
+        <div class="text-center w-full rounded-r-xl p-2 my-2 font-bold -ml-12">
+          {{motions[0].c}}
+        </div>  
       </div>
-      <div class="p-4 pl-6 my-2 font-bold text-sm text-left w-full rounded-xl cursor-pointer border border-gray-500 hover:bg-green-600 hover:border-yellow-500"
-      :class="this.$store.state.dark ? 'bg-gray-600 hover:bg-gray-600' : 'bg-gray-50 hover:bg-gray-200'">
-        {{motions[0].d}}
+      <div class="my-2 flex w-full rounded-xl cursor-pointer border hover:bg-green-500 hover:text-white"
+        :class="store.state.dark ? 'bg-gray-800 border-gray-600' : 'bg-gray-200 border-gray-400'">
+        <div class="rounded-l-xl p-4 align-text-middle font-semibold w-12"
+        :class="store.state.dark ? 'bg-black/20 border-r border-gray-600' : 'bg-white/20 border-r border-gray-400'">
+          D
+        </div>
+        <div class="text-center w-full rounded-r-xl p-2 my-2 font-bold -ml-12">
+          {{motions[0].d}}
+        </div>  
       </div>
+      <p class="mt-4 uppercase text-xs tracking-widest text-gray-400">
+        YOUR VOTE WILL BE REGISTERED IN THE BLOCKCHAIN
+      </p>
     </div>
-
     <div class="mt-8 text-center uppercase text-sm tracking-widest font-semibold justify-center">
       <div class="uppercase text-s tracking-widest text-gray-400 font-semibold text-center ">
-        CURRENT VOTING ENDS IN
+        CURRENT MOTION ENDS IN
       </div>
       <CountDown :target_date="motions[0].ends" class="text-2xl text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-green-400" />
+      <apexchart class="mt-6 flex justify-center" 
+      type="radialBar" height="150" width="150" 
+      :options="radialData.chartOptions" 
+      :series="radialData.series" />
+      <p class="uppercase text-sm tracking-widest text-gray-400 font-semibold">
+        PARTICIPATION
+      </p>
     </div>
 </div>
 
