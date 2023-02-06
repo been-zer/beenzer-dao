8c39c2f4d03cc5dd1e20701e07a865e9bcdd3071
quantium <alexcollsoutumuro@gmail.com>
2023-02-06 15:51:40 +0100

    add color btn

--- v2/src/services/store/index.ts
+++ v2/src/services/store/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createStore, Store } from 'vuex';
-import { user } from './user';
+import { createStore, Store } from "vuex";
+import { user } from "./user";
 // import { socket } from '../sockets';
 // import createSocketStore from '../../plugins/createSocketStore';
 
@@ -10,76 +10,76 @@ export const store: Store<any> = createStore({
   state: {
     users: 0,
     welcome: true,
-    pubkey: '',
-    username: '',
-    ip: '',
-    flag: '',
-    country: '',
-    city: '',
+    pubkey: "",
+    username: "",
+    userColor: "",
+    ip: "",
+    flag: "",
+    country: "",
+    city: "",
     newuser: true,
     singup: false,
     usernameAv: false,
     dark: true,
     sound: true,
     private: false,
-    usersFlags: []
+    usersFlags: [],
   },
   actions: {
-    nUsers (store, users: number) {
+    nUsers(store, users: number) {
       this.state.users = users;
     },
-    dispatchPubkey (store, pubkey: string) {
+    dispatchPubkey(store, pubkey: string) {
       this.state.pubkey = pubkey;
     },
-    dispatchUsername (store, username: string) {
+    dispatchUsername(store, username: string) {
       this.state.username = username;
     },
-    setIp (store, ip: string) {
+    setIp(store, ip: string) {
       this.state.ip = ip;
     },
-    setFlag (store, flag: string) {
+    setFlag(store, flag: string) {
       this.state.flag = flag;
     },
-    setCountry (store, country: string) {
+    setCountry(store, country: string) {
       this.state.country = country;
     },
-    setCity (store, city: string) {
+    setCity(store, city: string) {
       this.state.city = city;
     },
-    switchWelcome (store, to: boolean) {
+    switchWelcome(store, to: boolean) {
       this.state.welcome = to;
     },
-    switchUsernameAv (store, to: boolean) {
+    switchUsernameAv(store, to: boolean) {
       this.state.usernameAv = to;
     },
-    switchSignup (store, to: boolean) {
+    switchSignup(store, to: boolean) {
       this.state.singup = to;
     },
-    switchSingup () {
+    switchSingup() {
       this.state.singup = !this.state.singup;
     },
-    switchDark () {
+    switchDark() {
       this.state.dark = !this.state.dark;
     },
-    switchSound () {
+    switchSound() {
       this.state.sound = !this.state.sound;
     },
-    switchPrivate () {
+    switchPrivate() {
       this.state.private = !this.state.private;
     },
-    setUsersFlags (store, flags: Array<never>) {
+    setUsersFlags(store, flags: Array<never>) {
       this.state.usersFlags = flags;
-    }
+    },
   },
   modules: {
-    user
+    user,
   },
   plugins: [
     // socketPlugin
   ],
-})
+});
 
 export function useStore() {
   return store as Store<any>;
 }
-
