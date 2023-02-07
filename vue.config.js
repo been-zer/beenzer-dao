7533c4394f7ce38b6643a8eedcdf7fdaa528364b
quantium <alexcollsoutumuro@gmail.com>
2023-02-07 14:55:38 +0100

    add public in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
-  publicPath: path.join(__dirname, ""),
+  publicPath: path.join(__dirname, "public"),
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
