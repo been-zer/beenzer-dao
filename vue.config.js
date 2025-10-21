b04c6ac753541fc3f6c7ccb1ac0074ba59e24fc0
quantium-rock <alexcollsoutumuro@gmail.com>
2025-10-21 20:59:14 +0200

    🔧 Fix transpileDependencies syntax error

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,17 @@ const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
   publicPath: "",
-  transpileDependencies: [
-    // Exclude apexcharts from transpilation as it's already ESM and too large for Babel
-    (name) => !name.includes('apexcharts')
-  ],
+  transpileDependencies: [],
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
     stats: { warnings: false },
   },
   chainWebpack: (config) => {
     config.plugins.delete("friendly-errors");
+    // Exclude apexcharts from babel-loader to avoid large file processing
+    config.module
+      .rule("js")
+      .exclude.add(/node_modules[\/]apexcharts/)
+      .end();
   },
 });
