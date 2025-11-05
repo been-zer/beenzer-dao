cef83f015d127099a81856b3baddc8eff140de03
quantium-rock <alexcollsoutumuro@gmail.com>
2025-11-05 10:57:56 +0100

    🧰 CI build fix: disable webpack cache and Terser parallelism

--- vue.config.js
+++ vue.config.js
@@ -6,13 +6,28 @@ module.exports = defineConfig({
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
     stats: { warnings: false },
+    // Disable webpack persistent cache to avoid Cache.shutdown errors in CI
+    cache: false,
+    performance: { hints: false },
   },
   chainWebpack: (config) => {
     config.plugins.delete("friendly-errors");
+
     // Exclude apexcharts from babel-loader to avoid large file processing
     config.module
       .rule("js")
       .exclude.add(/node_modules[\/]apexcharts/)
       .end();
+
+    // Ensure Terser runs without worker threads (fixes CI/container issues)
+    config.optimization
+      .minimizer("terser")
+      .tap((args) => {
+        if (args && args[0]) {
+          args[0].parallel = false;
+          args[0].terserOptions = args[0].terserOptions || {};
+        }
+        return args;
+      });
   },
 });
